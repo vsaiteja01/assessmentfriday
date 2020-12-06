70598decd542aa71566b38ec6192512fd69d973b
vsaiteja01 <saitejatinkurocks@gmail.com>
2020-12-06 17:25:27 +0530

    second commit

---
+++ assignment/assignment/Models/Student.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace assignment.Models
+{
+    public class Student
+    {
+
+        public int SId { get; set; }
+        public int SName { get; set; }
+
+        public string SDOB { get; set; }
+
+        public string SContact { get; set; }
+
+
+    }
+}
